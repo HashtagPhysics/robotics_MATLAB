944d80706932eccc39f1f36ec33cb256e62928c7
Jim Goodall <james.a.goodall@gmail.com>
2025-03-07 15:12:32 -0500

    Update autobot.m

--- autobot.m
+++ autobot.m
@@ -7,18 +7,46 @@ driftOffset = -2.25;
 driveSpeed = 1;
 turnSpeed = 0.75;
 startTime = 0;
-startPos = "left";
+startPos = "center_L";
 
 switch startPos
-    case "center"
+    case "center_R"
         robot.y = 158.5;
         [robot, endTime]= driveStraight(robot,88,driveSpeed,startTime)
         % Drop coral
         [robot, endTime]= driveStraight(robot,-12,driveSpeed,endTime)
         [robot, endTime]= driveTurn(robot,90 + driftOffset,turnSpeed,endTime)
         [robot, endTime]= driveStraight(robot,60,driveSpeed,endTime)
-        [robot, endTime]= driveTurn(robot,-45 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,-90 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,120,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,-138 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,-98,driveSpeed,endTime)
+        % Load New Coral
+        [robot, endTime]= driveStraight(robot,98,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,-40 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,30,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,60 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,16,driveSpeed,endTime)
+        % Drop New Coral
+        
+    case "center_L"
+        robot.y = 158.5;
+        [robot, endTime]= driveStraight(robot,88,driveSpeed,startTime)
+        % Drop coral
+        [robot, endTime]= driveStraight(robot,-12,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,-90 + driftOffset,turnSpeed,endTime)
         [robot, endTime]= driveStraight(robot,60,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,90 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,120,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,138 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,-98,driveSpeed,endTime)
+        % Load New Coral
+        [robot, endTime]= driveStraight(robot,85,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,40 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,45,driveSpeed,endTime)
+        [robot, endTime]= driveTurn(robot,-35 + driftOffset,turnSpeed,endTime)
+        [robot, endTime]= driveStraight(robot,5,driveSpeed,endTime)
+        % Drop New Coral
         
     case "right"
         robot.y = 257.75;
